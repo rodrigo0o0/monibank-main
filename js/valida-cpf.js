a6009e5cb9eabe6ecd69c0baea9019e6f9e12009
rodrigo0o0 <sandeski47@gmail.com>
2023-05-13 07:45:50 -0300

    Projeto finalizado

---
+++ js/valida-cpf.js
@@ -0,0 +1,66 @@
+export default function ehUmCPF(campo){
+    const cpf = campo.value.replace(/\.|-/g, "");
+    if(validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)){
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
+
+
+}
+
+
+function validaNumerosRepetidos(cpf){
+    const numerosRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ];
+
+
+    return numerosRepetidos.includes(cpf);
+
+}
+
+
+function validaPrimeiroDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 10;
+  
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+      soma += parseInt(cpf[tamanho]) * multiplicador;
+      multiplicador--;
+    }
+  
+    soma = soma % 11;
+    soma = 11 - soma;
+    if (soma <= 2) {
+      soma = 0;
+    }
+    return soma != cpf[9];
+  }
+
+  function validaSegundoDigito(cpf) {
+    let soma = 0;
+    let multiplicador = 11;
+  
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+      soma += parseInt(cpf[tamanho]) * multiplicador;
+      multiplicador--;
+    }
+    
+    soma = soma % 11;
+    if(soma > 2){
+        soma = 11 - soma;
+
+    }else {
+      soma = 0;
+    }
+    return soma != parseInt(cpf[10]);
+  }
+  
